1a03409c35cefd1248cb2d116fa5743d6e61e6c2
sarthakshrm-dev <sarthakshrm2001@gmail.com>
2023-12-12 03:29:19 +0530

    updated

--- src/app/dashboard/events/page.js
+++ src/app/dashboard/events/page.js
@@ -3,11 +3,7 @@ import { EventsProfileView } from 'src/sections/events/view';
 // ----------------------------------------------------------------------
 
 export const metadata = {
-<<<<<<< HEAD
   title: 'Dashboard: Events Profile',
-=======
-  title: 'Dashboard: Events',
->>>>>>> 3158989336036a9443c48a8277182b1baf2e5a3a
 };
 
 export default function UserProfilePage() {
