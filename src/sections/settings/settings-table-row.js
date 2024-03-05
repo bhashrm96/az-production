73b5e2139ca0ef36382cd708fdd9b1ed2b568577
sarthakshrm-dev <sarthakshrm2001@gmail.com>
2024-03-05 08:14:02 +0530

    updated

--- src/sections/settings/settings-table-row.js
+++ src/sections/settings/settings-table-row.js
@@ -72,6 +72,7 @@ export default function SettingsTableRow({ row, selected, onEditRow, onSelectRow
           {stripHtmlTags(answer)}
         </TableCell>
 
+
         {/* <TableCell>
           <Label
             variant="soft"
